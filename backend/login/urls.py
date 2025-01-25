07debe784d57c61240e126812bea4acb096c57b8
RahulPurusothaman <rahul.p.ihub@snsgroups.com>
2025-01-25 15:22:53 +0530

    otp is wokring

--- backend/login/urls.py
+++ backend/login/urls.py
@@ -3,9 +3,6 @@ from . import views
 
 urlpatterns = [
     path('api/registration', views.login, name='login'),
-        # Endpoint to send OTP
     path('api/send_otp', views.send_otp, name='send_otp'),
-    
-    # Endpoint to verify OTP
     path('api/verify_otp', views.verify_otp, name='verify_otp'),
 ]
